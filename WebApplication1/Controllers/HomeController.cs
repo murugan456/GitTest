821c7521ac63cc6d83f418931a40657b629ebda0
Andrey Puchkov <andrey.puchkov@gmail.com>
2019-12-03 12:04:58 +0300

    sd

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
     public HomeController(ILogger<HomeController> logger)
     {
-      _logger = logger;
+      _logger = logger; 
     }
 
     public IActionResult Index()
